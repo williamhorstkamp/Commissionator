34a2e71e40a0f5dc41f8b1346d6bd5edeb2ec1b0
William Horstkamp <william.horstkamp@gmail.com>
2015-02-02 19:42:51 -0800

    Commissionator first upload
    Just includes visual studio files and the most recent version of SQLiter

---
+++ README.md
@@ -0,0 +1,17 @@
+# Commissionator
+Commissionator is a multi-platform utility for artists to document and track their commissions.
+
+##Copyright Stuff
+Copyright (c) 2015 William Horstkamp  
+william.horstkamp@gmail.com  
+MIT LICENSE  
+License info can be found at license.txt  
+
+The latest version of this code can always be found at  
+https://github.com/williamhorstkamp/Commissionator
+
+This code uses my SQLite3 wrapper as a base. It can always be found at
+https://github.com/williamhorstkamp/SQLiterForCpp11
+
+This code is dependant on the SQLite3 interface for C, which can be found at
+https://www.sqlite.org/download.html
---
+++ include/SQLiteException.h
@@ -0,0 +1,32 @@
+/**
+ *  SQLiteException.h
+ *  Simple exception object to be used by the rest of the library.
+ *
+ *  @author William Horstkamp
+ *  @version 1.0
+ */
+
+#include <stdexcept>
+
+namespace SQLiter{
+
+    /**
+     *  Simple exception class wraps an error message, typically one derived from
+     *  an SQLite3 result code, but also others such as file i/o exceptions thrown
+     *  by database create and open functions.
+     */
+
+    class SQLiteException : public std::runtime_error {
+    public:
+        /**
+         *  Constructor accepts a pointer to a C string containing the error
+         *  message. Hands that error message to parent constructor.
+         *
+         *  @param errMsg - Null terminated C String containing the error message
+         *      to built an SQLiteException around
+         *
+         *  @return - SQLiteException wrapping the error message to be thrown
+         */
+        SQLiteException(const char *errMsg) : std::runtime_error(errMsg) {};
+    };
+}
